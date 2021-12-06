331660b0284039bb9896fb49b62d5ad4a32116d9
huy75 <huy.nguyen@holbertonschool.com>
2021-12-06 10:14:21 +0000

    replace npm run with yarn & sub route & verbs

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import morgan from 'morgan'
 import cors from 'cors'
 
 export const app = express()
+const router = express.Router()
 
 app.disable('x-powered-by')
 
@@ -12,4 +13,42 @@ app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
-export const start = () => {}
+// middleware
+// const log = (req, res, next) => {
+//   console.log('logging')
+//   req.mydata = 'hello'
+//   next()
+// }
+
+router.get('/me', (req, res) => {
+  res.send({ message: 'hi' })
+})
+
+// use a sub route /api/me
+app.use('/api', router)
+
+// const routes = ['get /cat', 'get /cat/:id', 'post /cat', 'put /cat/:id', 'delete /cat/:id']
+
+router.route('/cat')
+  .get()
+  .post()
+
+router.route('cat /:id')
+  .get()
+  .put()
+  .delete()
+
+// app.get('/data', log, (req, res) => {
+app.get('/data', (req, res) => {
+  res.send({ message: req.mydata })
+})
+
+app.post('/data', (req, res) => {
+  res.send(req.body)
+})
+
+export const start = () => {
+  app.listen(3000, () => {
+    console.log('server is on 3000')
+  })
+}
